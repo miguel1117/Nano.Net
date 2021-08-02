164f05a7f6dae7743e18dc9ef721fac8431b3655
Miguel <34418635+miguel1117@users.noreply.github.com>
2021-08-02 01:49:38 +0100

    Small improvements to utils

--- Nano.Net/Utils.cs
+++ Nano.Net/Utils.cs
@@ -103,12 +103,14 @@ namespace Nano.Net
             return publicKeyBytes;
         }
 
-        internal static byte[] Blake2BHash(int sizeInBytes, byte[] data)
+        internal static byte[] Blake2BHash(int sizeInBytes, params byte[][] data)
         {
             Hasher hasher = Blake2B.Create(new Blake2BConfig() { OutputSizeInBytes = sizeInBytes });
-
             hasher.Init();
-            hasher.Update(data);
+
+            foreach (byte[] bytes in data)
+                hasher.Update(bytes);
+
             return hasher.Finish();
         }
 
@@ -123,7 +125,7 @@ namespace Nano.Net
 
             byte[] seedBytes = HexToBytes(seed);
 
-            return Blake2BHash(32, seedBytes.Concat(indexBytes).ToArray());
+            return Blake2BHash(32, seedBytes, indexBytes);
         }
 
         public static byte[] PublicKeyFromPrivateKey(byte[] privateKey)
