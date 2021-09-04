972325b681734797f317c4c7813c96a5c879f018
Miguel <34418635+miguel1117@users.noreply.github.com>
2021-09-04 12:34:22 +0100

    BlockBase now allows other types of blocks

--- Nano.Net/BlockBase.cs
+++ Nano.Net/BlockBase.cs
@@ -4,14 +4,28 @@ namespace Nano.Net
 {
     public class BlockBase
     {
-        [JsonProperty("type")] public string Type => "state";
-        [JsonProperty("account")] public string Account { get; init; }
-        [JsonProperty("previous")] public string Previous { get; init; }
-        [JsonProperty("representative")] public string Representative { get; init; }
-        [JsonProperty("balance")] public string Balance { get; init; }
-        [JsonProperty("link")] public string Link { get; init; }
-        
-        [JsonProperty("signature")] public string Signature { get; set; }
-        [JsonProperty("work")] public string Work { get; set; }
+        [JsonProperty("type")]
+        public string Type { get; set; } = "state";
+
+        [JsonProperty("account")]
+        public string Account { get; init; }
+
+        [JsonProperty("previous")]
+        public string Previous { get; init; }
+
+        [JsonProperty("representative")]
+        public string Representative { get; init; }
+
+        [JsonProperty("balance")]
+        public string Balance { get; init; }
+
+        [JsonProperty("link")]
+        public string Link { get; init; }
+
+        [JsonProperty("signature")]
+        public string Signature { get; set; }
+
+        [JsonProperty("work")]
+        public string Work { get; set; }
     }
 }
