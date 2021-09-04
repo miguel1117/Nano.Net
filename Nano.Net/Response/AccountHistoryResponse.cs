f9f359d86cda84ecd578e530547abc6528e88da4
Charterino <charterinod@gmail.com>
2021-09-04 11:04:14 +0000

    Added AccountHistoryResponse

---
+++ Nano.Net/Response/AccountHistoryResponse.cs
@@ -0,0 +1,31 @@
+using System.Collections.Generic;
+using Newtonsoft.Json;
+
+namespace Nano.Net
+{
+    public class AccountHistoryResponse
+    {
+        [JsonProperty("account")]
+        public string Account { get; set; }
+        [JsonProperty("history")]
+        public List<HistoryBlock> History { get; set; }
+        [JsonProperty("previous")]
+        public string Previous { get; set; }
+    }
+
+    public class HistoryBlock
+    {
+        [JsonProperty("type")]
+        public string Type { get; set; }
+        [JsonProperty("account")]
+        public string Account { get; set; }
+        [JsonProperty("amount")]
+        public string Amount { get; set; }
+        [JsonProperty("local_timestamp")]
+        public string LocalTimestamp { get; set; }
+        [JsonProperty("height")]
+        public ulong Height { get; set; }
+        [JsonProperty("hash")]
+        public string Hash { get; set; }
+    }
+}
