4987cadc54f37905fa80438f7f43c2efbb56113f
Charterino <charterinod@gmail.com>
2021-09-04 14:43:41 +0000

    Removed redundant code from BlockInfoResponse

--- Nano.Net/Response/BlockInfoResponse.cs
+++ Nano.Net/Response/BlockInfoResponse.cs
@@ -17,30 +17,8 @@ namespace Nano.Net
         [JsonProperty("confirmed")]
         public bool Confirmed { get; set; }
         [JsonProperty("contents")]
-        public BlockInfoResponseContent Content { get; set; }
+        public BlockBase Content { get; set; }
         [JsonProperty("subtype")]
         public string Subtype { get; set; }
-    }
-
-    public class BlockInfoResponseContent
-    {
-        [JsonProperty("type")]
-        public string Type { get; set; }
-        [JsonProperty("account")]
-        public string Account { get; set; }
-        [JsonProperty("previous")]
-        public string Previous { get; set; }
-        [JsonProperty("representative")]
-        public string Representative { get; set; }
-        [JsonProperty("balance")]
-        public string Balance { get; set; }
-        [JsonProperty("link")]
-        public string Link { get; set; }
-        [JsonProperty("link_as_account")]
-        public string LinkAsAccount { get; set; }
-        [JsonProperty("signature")]
-        public string Signature { get; set; }
-        [JsonProperty("work")]
-        public string Work { get; set; }
     }
 }
