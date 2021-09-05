d039fc5fe8a5d7972f7877926b5dad5571f62e75
Charterino <charterinod@gmail.com>
2021-09-05 11:17:58 +0000

    Added AccountsFrontiersResponse

---
+++ Nano.Net/Response/AccountsFrontiersResponse.cs
@@ -0,0 +1,11 @@
+using System.Collections.Generic;
+using Newtonsoft.Json;
+
+namespace Nano.Net
+{
+    public class AccountsFrontiersResponse
+    {
+        [JsonProperty("frontiers")]
+        public Dictionary<string, string> Frontiers { get; set; }
+    }
+}
