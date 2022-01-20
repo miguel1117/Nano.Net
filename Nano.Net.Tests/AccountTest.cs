6d1846d8b9268e6aa3b8bc069f7907e83118c9c3
Miguel <34418635+miguel1117@users.noreply.github.com>
2022-01-20 23:19:09 +0000

    Added account tests for the new public Account constructors

--- Nano.Net.Tests/AccountTest.cs
+++ Nano.Net.Tests/AccountTest.cs
@@ -21,5 +21,16 @@ namespace Nano.Net.Tests
             var account = Account.FromPrivateKey(privateKey);
             Assert.Equal("nano_3fkaipau6kpfejzsr6c4itkm3zkf3jqd9dxkccr3so5fbypop4uu9f4fcacz",account.Address);
         }
+
+        [Fact]
+        public void NewAccountTest()
+        {
+            const string seed = "7B58BB18E887F5146DD46ACAD89E5139D363F5F4A50DE9A0B465217A51D1BFDF";
+
+            var account = new Account(seed, 63);
+            Assert.Equal(Utils.HexToBytes("003D0478B4E3F01889A0E9BB149C701E90EFC0E1BC1702671AA9790F71FED69E"), account.PrivateKey);
+            Assert.Equal(Utils.HexToBytes("83778F31E96011CC3C132680A512FECC24E33174F4FB8443A583D6B30F744AAA"), account.PublicKey);
+            Assert.Equal("nano_31uqjwrykr1jsiy38bn1nnbhxm36werqbx9uij3td1yppe9qakocutbzzmd9", account.Address);
+        }
     }
 }
