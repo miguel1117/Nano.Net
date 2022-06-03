99047808472d224207a7cab04e4c8e9a20a34b56
Miguel <34418635+miguel1117@users.noreply.github.com>
2022-06-03 21:41:12 +0100

    Update BlockTest.cs

--- Nano.Net.Tests/BlockTest.cs
+++ Nano.Net.Tests/BlockTest.cs
@@ -11,7 +11,7 @@ public class BlockTest
         Account = "nano_3tjhazni9juaoa8q9rw33nf3f6i45gswhpzrgrbrawxhh7a777ror9okstch",
         Previous = "3865BFCD423CE3579C4A7C6010CE763BE4C63964AC06BDA451A63BBCAC9E3712",
         Representative = "nano_18shbirtzhmkf7166h39nowj9c9zrpufeg75bkbyoobqwf1iu3srfm9eo3pz",
-        Balance = "364448673",
+        Balance = "33022",
         Link = "nano_3tjhazni9juaoa8q9rw33nf3f6i45gswhpzrgrbrawxhh7a777ror9okstch",
         Subtype = BlockSubtype.Send
     };
@@ -19,7 +19,7 @@ public class BlockTest
     [Fact]
     public void HashBlock_ValidateHash_ValidHash()
     {
-        Assert.Equal("66A580645BEE693103CDE784764043092F7CB966B44C4029576C5CE17C222F40", _testBlock.Hash, true);
+        Assert.Equal("F19FCBB3145E6A54D6390016245B2FDDCD864DDD5CA04961F05B6F56C3FD28C3", _testBlock.Hash, true);
     }
 
     [Fact]
