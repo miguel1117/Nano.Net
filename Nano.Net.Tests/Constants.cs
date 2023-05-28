98d6be0cd9c8c79cffff1215d288e440846e5ced
Miguel <34418635+miguel1117@users.noreply.github.com>
2023-05-28 22:46:43 +0100

    Re-enable WebSockets tests with an updated node URL

--- Nano.Net.Tests/Constants.cs
+++ Nano.Net.Tests/Constants.cs
@@ -4,5 +4,5 @@ internal static class Constants
 {
     internal const string ReferenceAccount = "nano_3r9rdhbipf9xsnpxdhf7h7kebo8iyfefc9s3bcx4racody5wubz1y1kzaon9"; // static burn account used for testing
     internal const string RpcAddress = "https://node.shrynode.me/api";
-    internal const string WebSocketAddress = "wss://ws.mynano.ninja/";
+    internal const string WebSocketAddress = "wss://node.somenano.com/websocket";
 }
--- Nano.Net.Tests/WebSocketsTest.cs
+++ Nano.Net.Tests/WebSocketsTest.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Threading.Tasks;
 using Nano.Net.WebSockets;
 using Xunit;
@@ -6,33 +7,35 @@ namespace Nano.Net.Tests;
 
 public class WebSocketFixture : IAsyncLifetime
 {
-    public NanoWebSocketClient Client;
+    public readonly NanoWebSocketClient Client = new NanoWebSocketClient(Constants.WebSocketAddress);
 
     public async Task InitializeAsync()
     {
-        Client = new NanoWebSocketClient(Constants.WebSocketAddress);
-        await Client.Start();
+        await Client.Start().ConfigureAwait(false);
+        Console.WriteLine();
     }
 
     public async Task DisposeAsync()
     {
+        await Client.Stop().ConfigureAwait(false);
+        Client.Dispose();
     }
 }
 
-// public class WebSocketsTest : IClassFixture<WebSocketFixture>
-// {
-//     private readonly WebSocketFixture _fixture;
-//
-//     public WebSocketsTest(WebSocketFixture fixture)
-//     {
-//         _fixture = fixture;
-//     }
-//
-//     [Fact(Skip = "anyways...")]
-//     public async void WebSocketPing_CheckResponse_IsNotNull()
-//     {
-//         PingMessage ping = await _fixture.Client.Ping();
-//
-//         Assert.NotNull(ping.Ack);
-//     }
-// }
+public class WebSocketsTest : IClassFixture<WebSocketFixture>
+{
+    private readonly WebSocketFixture _fixture;
+
+    public WebSocketsTest(WebSocketFixture fixture)
+    {
+        _fixture = fixture;
+    }
+
+    [Fact]
+    public async void WebSocketPing_CheckResponse_IsNotNull()
+    {
+        PingMessage ping = await _fixture.Client.Ping().ConfigureAwait(false);
+
+        Assert.NotNull(ping.Ack);
+    }
+}
