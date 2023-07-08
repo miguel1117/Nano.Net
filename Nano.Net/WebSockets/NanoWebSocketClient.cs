bb95f87ec3f96d5f67dd969282db09a0ebd7d04f
PNTX <bolter.daniel@hotmail.com>
2023-07-08 12:01:36 +0200

    removed unnecessary using

--- Nano.Net/WebSockets/NanoWebSocketClient.cs
+++ Nano.Net/WebSockets/NanoWebSocketClient.cs
@@ -1,8 +1,7 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net.WebSockets;
-using System.Threading;
 using System.Threading.Tasks;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
